df8b2eec3d0b6627a6e0e9bdec7077e3a35475d1
Sergei Student <lsl1959@mail.ru>
2023-02-09 20:40:07 +0600

    finish footer and script

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,4 +1,28 @@
-// Підключення функціоналу "Чертоги Фрілансера"
+// Подключение функционала "Чертоги Фрилансера"
 import { isMobile } from "./functions.js";
-// Підключення списку активних модулів
+// Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+"use script"
+
+
+window.addEventListener('load', windowLoad);
+
+function windowLoad() {
+
+	document.body.classList.add('loaded');
+
+	document.addEventListener("click", documentActions);
+
+	function  documentActions(e) {
+		const targetElement = e.target;
+		//Tabs - выполняется табуляция по категории
+		// Up - страница скролится к шапке при нажатии на кнопку Up в footer
+		if(targetElement.closest('.footer__up')) {
+			window.scrollTo({
+				top:0,
+				behavior: "smooth"
+			});
+			e.preventDefault();
+		}
+	}
+}
